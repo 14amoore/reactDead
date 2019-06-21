95a189d22a64a2f03d8ab9d54bda4cc488922c99
Adam Moore <47046089+14amoore@users.noreply.github.com>
2019-06-21 13:23:24 -0400

    Add files via upload

--- precache-manifest.1f5648537694a9f751400862008ee507.js
+++ precache-manifest.1f5648537694a9f751400862008ee507.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "14df5ec86a1c9c33afba2e4aa76b0dc2",
-    "url": "/14amoore/reactDead/index.html"
+    revision: '14df5ec86a1c9c33afba2e4aa76b0dc2',
+    url: '/reactDead/index.html'
   },
   {
-    "revision": "65e316c574292553c79c",
-    "url": "/14amoore/reactDead/static/css/main.95f782c1.chunk.css"
+    revision: '65e316c574292553c79c',
+    url: '/reactDead/static/css/main.95f782c1.chunk.css'
   },
   {
-    "revision": "8d7e71fc5f551f7cf40d",
-    "url": "/14amoore/reactDead/static/js/2.b85fd010.chunk.js"
+    revision: '8d7e71fc5f551f7cf40d',
+    url: '/reactDead/static/js/2.b85fd010.chunk.js'
   },
   {
-    "revision": "65e316c574292553c79c",
-    "url": "/14amoore/reactDead/static/js/main.58c65555.chunk.js"
+    revision: '65e316c574292553c79c',
+    url: '/reactDead/static/js/main.58c65555.chunk.js'
   },
   {
-    "revision": "12137d44e5a8b381015e",
-    "url": "/14amoore/reactDead/static/js/runtime~main.fee6b2b2.js"
+    revision: '12137d44e5a8b381015e',
+    url: 'reactDead/static/js/runtime~main.fee6b2b2.js'
   },
   {
-    "revision": "0a5ede36d6fdd8ea5adb51208b15376b",
-    "url": "/14amoore/reactDead/static/media/stealy.0a5ede36.png"
+    revision: '0a5ede36d6fdd8ea5adb51208b15376b',
+    url: '/reactDead/static/media/stealy.0a5ede36.png'
   }
 ]);
